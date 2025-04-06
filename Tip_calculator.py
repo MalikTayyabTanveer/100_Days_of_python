8400e9e87e6dc34fc691ca47826787e7565a1461
Malik Tayyab Tanveer <138617303+MalikTayyabTanveer@users.noreply.github.com>
2025-04-06 22:26:42 +0500

    Initial commit

---
+++ Tip_calculator.py
@@ -0,0 +1,13 @@
+print("welcoome to the tip calculator")
+
+total_bill = float(input("what is your total bill? $"))
+tip = int(input("how much would like to give tip? 10, 12 or 15? "))
+
+total_bill += total_bill * (tip / 100)
+print(total_bill) 
+
+person = int(input("how many people to split the bill? "))
+
+total_bill = round(total_bill / person,2)
+
+print(f"each person should pay: ${total_bill}")
---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+city_name = input("what is your city name\n")
+pet_name = input("what is your pet name\n")
+
+print("your band name should be " + city_name + " " + pet_name)
